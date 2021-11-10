6cf373be2cdbde87fee27dbaed256f77d1daf3c9
noloerino <jhshi07@gmail.com>
2021-11-09 22:52:47 -0800

    add duplication to bench script

--- benchmark/base_bench.py
+++ benchmark/base_bench.py
@@ -3,6 +3,8 @@ Benchmarking stuff, slightly modified from
 https://github.com/modin-project/df-bench/blob/master/benchmark/base_bench.py
 """
 
+import os
+
 # Original csv is about 70 MB
 dup_counts = [1, 3, 10]
 test_files = [(f"dup_{i}_" if i != 1 else "") + "fhv_tripdata_2021-07.csv" for i in dup_counts]
@@ -11,13 +13,22 @@ c_DOLocation = "DOLocationID"
 c_dropoff_datetime = "dropoff_datetime"
 c_PULocation = "PULocationID"
 
-
 class BaseBenchmark:
     """
     All methods in this class should be shadowed in the subclass so the benchmark
     output is more descriptive - e.g. ``benchmark(_test_read_csv, file)`` should be
     called in the child by a function called ``test_modin_read_csv``.
     """
+
+    # Handle for "pandas" module
+    _lib = None
+
+    @classmethod
+    def setup_class(cls):
+        for i, fn in zip(dup_counts, test_files):
+            if not os.path.exists(fn):
+                os.system(f"./repeat_csv.sh fhv_tripdata_2021-07.csv {i}")
+
 
     def _do_read_csv(self, file):
         df = self._lib.read_csv(file)
@@ -38,4 +49,3 @@ class BaseBenchmark:
         t1 = df[pulocation.notna()]
         dolocation = t1[c_DOLocation]
         return t1[dolocation.notna()]
-
